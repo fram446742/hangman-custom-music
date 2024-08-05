3a384f54d8a8c878462889fc6a0c26e08ae58f2b
Francisco Martin Velez Manrique <42354796+fram446742@users.noreply.github.com>
2024-08-05 19:04:47 +0200

    migrated from lazystatic to once-cell

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,20 @@
 use consts::{Hangman, INFO_DICT};
-use lazy_static::lazy_static;
 use logic::game;
 use std::collections::{HashMap, HashSet};
 use std::sync::Mutex;
 use termcolor::{Color, ColorChoice, StandardStream};
 use tools::{clear, get_color, get_difficulty, random_color};
+use once_cell::sync::Lazy;
 mod consts;
 mod logic;
 mod tools;
 
-lazy_static! {
-    pub static ref COLOR: Mutex<Color> = Mutex::new(random_color());
-    pub static ref LANGUAGE: Mutex<bool> = Mutex::new(false);
-    pub static ref DICTIONARY: Mutex<HashMap<u8, &'static str>> =
-        Mutex::new(INFO_DICT.iter().cloned().collect());
-    pub static ref DIFFICULTY: Mutex<u8> = Mutex::new(4);
-    pub static ref NUM_PLAYERS: Mutex<u8> = Mutex::new(1);
-}
+static COLOR: Lazy<Mutex<Color>> = Lazy::new(|| Mutex::new(random_color()));
+static LANGUAGE: Lazy<Mutex<bool>> = Lazy::new(|| Mutex::new(false));
+static DICTIONARY: Lazy<Mutex<HashMap<u8, &'static str>>> =
+    Lazy::new(|| Mutex::new(INFO_DICT.iter().cloned().collect()));
+static DIFFICULTY: Lazy<Mutex<u8>> = Lazy::new(|| Mutex::new(4));
+static NUM_PLAYERS: Lazy<Mutex<u8>> = Lazy::new(|| Mutex::new(1));
 
 fn main() {
     clear();
